e356d1ca7a76e6d308c828ddd9f3ad4d17a3ee59
Nicolas Pierre <nico_kestens@hotmail.com>
2024-01-03 17:11:34 +0100

    Add Licensing Dialog

--- src/TheTvdbDotNet/Http/AuthenticatedTvdbHttpClient.cs
+++ src/TheTvdbDotNet/Http/AuthenticatedTvdbHttpClient.cs
@@ -2,35 +2,28 @@
 using System.Threading.Tasks;
 using TheTvdbDotNet.Authentication;
 
-namespace TheTvdbDotNet.Http
+namespace TheTvdbDotNet.Http;
+
+public class AuthenticatedTvdbHttpClient(ITvdbHttpClient httpClient, IAuthenticator authenticator) : IAuthenticatedTvdbHttpClient
 {
-    public class AuthenticatedTvdbHttpClient : IAuthenticatedTvdbHttpClient
+    private readonly ITvdbHttpClient httpClient = httpClient;
+    private readonly IAuthenticator authenticator = authenticator;
+
+    public async Task<T> GetAsync<T>(Request request)
     {
-        private readonly ITvdbHttpClient httpClient;
-        private readonly IAuthenticator authenticator;
+        await authenticator.AuthenticateIfNecessaryAsync().ConfigureAwait(false);
+        return await httpClient.GetResponseAsync<T>(request.BuildRequest()).ConfigureAwait(false);
+    }
 
-        public AuthenticatedTvdbHttpClient(ITvdbHttpClient httpClient, IAuthenticator authenticator)
-        {
-            this.httpClient = httpClient;
-            this.authenticator = authenticator;
-        }
+    public async Task<T> PostAsync<T>(Request request, object postData)
+    {
+        await authenticator.AuthenticateIfNecessaryAsync().ConfigureAwait(false);
+        return await httpClient.PostResponseAsync<T>(request.BuildRequest(), postData).ConfigureAwait(false);
+    }
 
-        public async Task<T> GetAsync<T>(Request request)
-        {
-            await authenticator.AuthenticateIfNecessaryAsync().ConfigureAwait(false);
-            return await httpClient.GetResponseAsync<T>(request.BuildRequest()).ConfigureAwait(false);
-        }
-
-        public async Task<T> PostAsync<T>(Request request, object postData)
-        {
-            await authenticator.AuthenticateIfNecessaryAsync().ConfigureAwait(false);
-            return await httpClient.PostResponseAsync<T>(request.BuildRequest(), postData).ConfigureAwait(false);
-        }
-
-        public async Task<Stream> GetStreamAsync(Request request)
-        {
-            await authenticator.AuthenticateIfNecessaryAsync().ConfigureAwait(false);
-            return await httpClient.GetStreamAsync(request.BuildRequest()).ConfigureAwait(false);
-        }
+    public async Task<Stream> GetStreamAsync(Request request)
+    {
+        await authenticator.AuthenticateIfNecessaryAsync().ConfigureAwait(false);
+        return await httpClient.GetStreamAsync(request.BuildRequest()).ConfigureAwait(false);
     }
 }
